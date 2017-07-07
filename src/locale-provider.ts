43f4ed024baa94365ae4d9bf4ad182679f05993d
Matthias Buchholz <matthias@afkm.io>
2017-07-07 11:19:53 +0200

    kio-ng2 dependency

--- src/locale-provider.ts
+++ src/locale-provider.ts
@@ -1,7 +1,7 @@
 import { InjectionToken, Provider } from '@angular/core'
 import { LocaleProvider } from './interfaces/locale-provider'
 
-export let LOCALE_PROVIDER = new InjectionToken<LocaleProvider<string>>('locale_provider')
+export let LOCALE_PROVIDER = new InjectionToken<LocaleProvider>('locale_provider')
 
 export let DefaultLocaleProvider:Provider = {
   provide: LOCALE_PROVIDER,
