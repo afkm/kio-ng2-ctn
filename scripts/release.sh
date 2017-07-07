5d17436f8d25eef49fa11e8a2ac22dcd9dfe0b56
Matthias Buchholz <matthias@afkm.io>
2017-07-07 16:42:12 +0200

    release script

---
+++ scripts/release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+MODULE_ROOT="$(cd "$(dirname "${0}")/.."; pwd)"
+
+cd $MODULE_ROOT
+
+git checkout master
+git merge development
+
+npm version ${1:-patch}
+
+git push && git push --tags
